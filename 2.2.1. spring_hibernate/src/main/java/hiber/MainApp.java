3816b8f9a4c8db38026529ff10ce42ea4a4fc1f0
UnsleepingOwl <lion2000_07@mail.ru>
2022-11-08 14:11:10 +0300

    Task completed

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -1,6 +1,7 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -9,26 +10,49 @@ import java.sql.SQLException;
 import java.util.List;
 
 public class MainApp {
-   public static void main(String[] args) throws SQLException {
-      AnnotationConfigApplicationContext context = 
-            new AnnotationConfigApplicationContext(AppConfig.class);
+    public static void main(String[] args) throws SQLException {
 
-      UserService userService = context.getBean(UserService.class);
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+        UserService userService = context.getBean(UserService.class);
 
-      List<User> users = userService.listUsers();
-      for (User user : users) {
-         System.out.println("Id = "+user.getId());
-         System.out.println("First Name = "+user.getFirstName());
-         System.out.println("Last Name = "+user.getLastName());
-         System.out.println("Email = "+user.getEmail());
-         System.out.println();
-      }
+        Car car1 = new Car(Math.abs((int) (System.nanoTime())), "Machine infernale"),
+                car2 = new Car(Math.abs((int) (System.nanoTime())), "Machine (non) infernale");
 
-      context.close();
-   }
+        // Перегруженный конструктор
+//        User user1 = new User("Пафнутий", "Ложечкин", "palozh@bk.ru", car1),
+//                user2 = new User("Даздраперма", "Кукушкина", "dazdraku@mail.ru", car2);
+
+
+        // Обычный конструктор, добавление машин через сеттеры
+        User user1 = new User("Пафнутий", "Ложечкин", "palozh@bk.ru"),
+                user2 = new User("Даздраперма", "Кукушкина", "dazdraku@mail.ru");
+
+        user1.setCar(car1);
+        user2.setCar(car2);
+
+
+        // Скармливание связки User/Car базе данных
+        userService.add(user1);
+        userService.add(user2);
+
+        // Вывод списка User из базы данных
+        System.out.println("------------------------------------------------------------");
+        System.out.println("Список пользователей:");
+        List<User> users = userService.listUsers();
+        for (User user : users) {
+            System.out.println(user);
+        }
+        System.out.println("------------------------------------------------------------");
+
+        // Получение User из базы данных по связке model/car_series
+        System.out.println("------------------------------------------------------------");
+        System.out.println("Нахождение пользователя по связке модель/серийный номер:");
+        System.out.println(userService.getUserByCarModelAndSeries(car1.getModel(), car1.getSeries()));
+        System.out.println(userService.getUserByCarModelAndSeries(car2.getModel(), car2.getSeries()));
+        System.out.println("------------------------------------------------------------");
+
+        context.close();
+    }
 }
--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -1,29 +1,47 @@
 package hiber.dao;
 
 import hiber.model.User;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.TypedQuery;
+import java.sql.SQLException;
 import java.util.List;
 
 @Repository
 public class UserDaoImp implements UserDao {
 
-   @Autowired
-   private SessionFactory sessionFactory;
+    @Autowired
+    private SessionFactory sessionFactory;
 
-   @Override
-   public void add(User user) {
-      sessionFactory.getCurrentSession().save(user);
-   }
+    @Override
+    public User getUserByCarModelAndSeries(String model, int series) {
+        User user;
+        try {
+            user = (User) sessionFactory
+                    .getCurrentSession()
+                    .createQuery("from User where car.model = :model and car.series = :series")
+                    .setParameter("model", model)
+                    .setParameter("series", series)
+                    .getSingleResult();
+        } catch (Exception e) {
+            throw new RuntimeException("В базе данных нет такой комбинации модель/серийный номер");
+        }
+        return user;
+    }
 
-   @Override
-   @SuppressWarnings("unchecked")
-   public List<User> listUsers() {
-      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
-      return query.getResultList();
-   }
+    @Override
+    public void add(User user) {
+        sessionFactory.getCurrentSession().save(user);
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<User> listUsers() {
+        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("from User");
+        return query.getResultList();
+    }
 
 }
